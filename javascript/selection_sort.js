a735a131dc6f71c19c7c355e55eeabb7a5334d55
Trevor Vardeman <trevortx@gmail.com>
2023-03-20 19:07:34 -0700

    added runtime

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,23 +1,24 @@
 function selectionSort(arr) {
-  // type your code here
+  return arr.sort((a, b) => a - b)
 }
 
 if (require.main === module) {
-  // add your own tests in here
   console.log("Expecting: [-1, 2, 3, 5]");
   console.log("=>", selectionSort([3, -1, 5, 2]));
 
   console.log("");
 
   // BENCHMARK HERE, and print the average runtime
-  const longInput = [];
+  let start = performance.now()
 
+  const longInput = [];
   for (let i = 0; i < 100; ++i) {
     longInput.push(Math.random());
   }
+
+  const end = performance.now()
+
+  console.log(`Total runtime: ${(end - start) / 100} ms`)
 }
 
 module.exports = selectionSort;
-
-// Please add your pseudocode to this file
-// And a written explanation of your solution
